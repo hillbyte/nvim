f53fc129dd1b0a1acc48b7057503087f4afbe723
Abdulla Hill Baky <ahillbaky@gmail.com>
2024-08-04 20:44:12 +0530

    lsp config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,4 +8,19 @@ return {
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
   },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config=function()
+      require("telescope").setup( {
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {}
+          }
+  }
+})
+-- To get ui-select loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+   require("telescope").load_extension("ui-select")
+    end
+  }
 }
