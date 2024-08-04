6fc1e0c8e1a81c01b8f605ec6ff2412c2481fb2b
Abdulla Hill Baky <ahillbaky@gmail.com>
2024-08-05 01:27:44 +0530

    lintier conf fix

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,8 +7,7 @@ return {
         null_ls.builtins.formatting.stylua,
         --null_ls.builtins.completion.spell,
         null_ls.builtins.formatting.prettier,
-null_ls.builtins.diagnostics.eslint_d,
-
+          null_ls.builtins.diagnostics.erb_lint,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
